7876cfc7d5e3baf14dd8bb2d5ddc6aa1bfd73721
Kavish Singla <singlakavish09@gmail.com>
2025-12-09 02:00:38 +0530

    components added

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,11 +1,11 @@
 export function InputBox({label , placeholder}){
     return(
         <div>
-            <div className="text-sm font-medium text-left py-2">
+            <div className="text-xl font-medium text-left py-2">
                 {label}
             </div>
             <div >
-                <input type="text" placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
+                <input type="text" placeholder={placeholder} className="w-full text-2xl px-2 py-1 border rounded border-slate-200" />
             </div>
         </div>
     )
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,28 +1,24 @@
-import { BottomWarning } from "../components/BottomWarning";
-import { Button } from "../components/Button";
-import { Heading } from "../components/Heading";
-import { InputBox } from "../components/InputBox";
-import { SubHeading } from "../components/SubHeading";
+import { BottomWarning } from "../components/BottomWarning"
+import { Button } from "../components/Button"
+import { Heading } from "../components/Heading"
+import { InputBox } from "../components/InputBox"
+import { SubHeading } from "../components/SubHeading"
 
-
-export function Signup() {
-    return (
-        <div className="bg-slate-300 h-screen flex justify-center">
-
-            <div className="flex flex-col justify-center">
-
-                <div></div>
-
-            </div>
-           
+export const Signup = () => {
+    return <div className="bg-slate-300 h-screen flex justify-center">
+    <div className="flex flex-col justify-center w-lg">
+      <div className="rounded-lg bg-white  w-full text-center p-4 h-max px-4">
+        <Heading label={"Sign up"} />
+        <SubHeading label={"Enter your infromation to create an account"} />
+        <InputBox placeholder="John" label={"First Name"} />
+        <InputBox placeholder="Doe" label={"Last Name"} />
+        <InputBox placeholder="example@gmail.com" label={"Email"} />
+        <InputBox placeholder="123456" label={"Password"} />
+        <div className="pt-4">
+          <Button label={"Sign up"} />
         </div>
-    )
+        <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
+      </div>
+    </div>
+  </div>
 }
-
-
-
-//  <Heading label="Sign Up"></Heading>
-//             <SubHeading subheading="Enter your information to create an account."></SubHeading>
-//             <InputBox label="name" placeholder="Jhon" ></InputBox>
-//             <Button label="Sign Up" ></Button>
-//             <BottomWarning label="Already have an account ?" buttonText="Sign In" ></BottomWarning>
