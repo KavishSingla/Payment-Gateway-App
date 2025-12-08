3b64dd73af78858e47db771cb1c98fcd770545ea
Kavish Singla <singlakavish09@gmail.com>
2025-12-09 01:14:40 +0530

    some frontend added

---
+++ frontend/src/pages/Signin.jsx
@@ -0,0 +1,12 @@
+import { Heading } from "../components/Heading";
+import { SubHeading } from "../components/SubHeading";
+
+
+export function Signin() {
+    return (
+        <div>
+        <Heading label="hello"></Heading>
+        <SubHeading label = "Enter your information to create an account"></SubHeading>
+        </div>
+    )     
+}
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,28 @@
+import { BottomWarning } from "../components/BottomWarning";
+import { Button } from "../components/Button";
+import { Heading } from "../components/Heading";
+import { InputBox } from "../components/InputBox";
+import { SubHeading } from "../components/SubHeading";
+
+
+export function Signup() {
+    return (
+        <div className="bg-slate-300 h-screen flex justify-center">
+
+            <div className="flex flex-col justify-center">
+
+                <div></div>
+
+            </div>
+           
+        </div>
+    )
+}
+
+
+
+//  <Heading label="Sign Up"></Heading>
+//             <SubHeading subheading="Enter your information to create an account."></SubHeading>
+//             <InputBox label="name" placeholder="Jhon" ></InputBox>
+//             <Button label="Sign Up" ></Button>
+//             <BottomWarning label="Already have an account ?" buttonText="Sign In" ></BottomWarning>
